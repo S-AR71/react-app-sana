5a5fe4f6c80495bb4427b3315e2a24c9955421f4
S-AR71 <116632150+S-AR71@users.noreply.github.com>
2023-12-07 14:41:06 +0100

    Initial commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,33 @@
+import { MouseEvent } from "react";
+
+function ListGroup() {
+  let items = ["Kuala Lumpar", "Konya", "Karachi", "Kyoto"];
+  let selectedIndex = 0;
+
+  // Event Handler
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
